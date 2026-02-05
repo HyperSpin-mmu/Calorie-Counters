752152b889b048fa237eb8bec2c81eadc3735a7e
Ben Rantin <23761571@stu.mmu.ac.uk>
2026-02-05 15:51:24 +0000

    Changed the basic text just so show that it works

--- Calorie-Counter/app/index.tsx
+++ Calorie-Counter/app/index.tsx
@@ -9,7 +9,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Count your calories!</Text>
+      <Text>This is the main screen of the Calorie Counter app.</Text>
     </View>
   );
 }
