e2cb22f54acda9739d9ea0d52e18f18db1810f76
HyperSpin-mmu <23761571@stu.mmu.ac.uk>
2026-02-09 15:27:43 +0000

    I just added fonts and started implementing tabs into the app. So theres a home page and then there's a search button. They both link to the correct page due to the Tabs func in Layout

--- Calorie-Counter/app/_layout.tsx
+++ Calorie-Counter/app/_layout.tsx
@@ -1,5 +1,42 @@
-import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    'GoogleSans': require("../assets/fonts/GoogleSans-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  return (
+    <Tabs 
+      screenOptions={{ 
+        headerShown: false,
+        tabBarActiveTintColor: '#262525', 
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Dashboard',
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="search" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
