9f36805e315d5c39a9903b9c8e5c7bf10b29393c
HyperSpin-mmu <23761571@stu.mmu.ac.uk>
2026-02-12 14:59:24 +0000

    Added barcode scanner to on press for button so now barcode scanner shows up and is able to scan items, then returns data. Gets sent back to main page but will be directed to a portion page and call the api etc...

--- Calorie-Counter/app/action.tsx
+++ Calorie-Counter/app/action.tsx
@@ -1,21 +1,91 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Tabs } from 'expo-router';
+import { useRouter } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { CameraView, CameraType, useCameraPermissions, BarcodeScanningResult } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
 
-export default function SearchScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Action!</Text>
-    </View>
-  );
+export default function barcodeScanner() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [barcodeId, setBarcodeId] = useState<string | null>(null);
+  const router = useRouter();
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  const handleBarcodeScanned = ({ type, data }: BarcodeScanningResult) => {
+    setBarcodeId(data)
+  };
+
+return (
+  <View style={styles.container}>
+    {!barcodeId ? (
+      <CameraView 
+        style={styles.camera} 
+        facing={'back'}
+        onBarcodeScanned={handleBarcodeScanned}
+        barcodeScannerSettings={{
+          barcodeTypes: ["ean8","ean13"],
+        }}
+      />
+    ) : (
+      <View style={styles.resultContainer}>
+        <Text>Scanned ID: {barcodeId}</Text>
+        <Button title="Scan Again" onPress={() => setBarcodeId(null)} />
+        router.navigate('/index')
+      </View>
+    )}
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
   },
-  title: {
-    fontSize: 28,
-    fontFamily: "GoogleSans",
-  }
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  resultContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 64,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    width: '100%',
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
