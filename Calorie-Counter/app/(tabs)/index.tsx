52efd069b93d938c608b7b6d678b7162c81dbc4a
KurtAntAbano <ka04122005@gmail.com>
2026-02-17 21:49:51 +0000

    Updated index.tsx to import Firebase auth and retrieve the current user. The logged in users email is displayed (This will later be replaced with the users name).

--- Calorie-Counter/app/(tabs)/index.tsx
+++ Calorie-Counter/app/(tabs)/index.tsx
@@ -1,10 +1,18 @@
 import { Text, View, StyleSheet } from "react-native";
+import { auth } from "../../firebase"; // added import
 
 // This is the main page of the app, it is the default screen that is shown when the app is opened.
 export default function Index() {
+  const user = auth.currentUser; // added user variable
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Main page</Text>
+
+      {/* Print the email below */}
+      {user?.email && (
+        <Text style={styles.email}>{user.email}</Text>
+      )}
     </View>
   );
 }
@@ -22,5 +30,11 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontFamily: "GoogleSans",
     textAlign: "center",
-  }
+  },
+  email: {
+    fontSize: 20,
+    color: "gray",
+    marginTop: 10,
+    fontFamily: "GoogleSans",
+  },
 });
