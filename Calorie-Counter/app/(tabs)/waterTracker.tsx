4198a5ece3ba7eba290c72aac116ebaacf5f04c6
FizzinqFizq <fizashiraz@outlook.com>
2026-03-19 13:20:35 +0000

    Final corrected conditional logic

--- Calorie-Counter/app/(tabs)/waterTracker.tsx
+++ Calorie-Counter/app/(tabs)/waterTracker.tsx
@@ -4,28 +4,30 @@ import { Alert, Button, Text, View, StyleSheet, TextInput } from "react-native";
 export default function WaterTracker() {
   // Create two condition variable: true and false
   // using State
-  const True = useState("")
-  const False = useState("home")
+  const [isLogged, setIsLogged] = useState(false) 
 
   // Using simple condtion (the if statement):
   // If the condition is true, show the screen to input the water (UI)
   // Else, return to the home screen
-  if (True)
+  if (isLogged) // -> setIsLogged(true)
   {
     return (
       <View>
         <TextInput placeholder="e.g., 250ml" />
         <Button title="Enter" onPress={() => Alert.alert("Submitted")} />
+        <Button title="Back" onPress={() => setIsLogged(false)} />
       </View>
     )
   }
   
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Water Log</Text>
-      <Button title="Log" onPress={() => useState(True)} />
-    </View>
-  );
+  else {    
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Water Log</Text>
+        <Button title="Log" onPress={() => setIsLogged(true)} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
